7f1a804b161403ba252d2e29caa622537a140a99
Iago Alves Brito <71942285+iagoalvesb@users.noreply.github.com>
2025-11-16 06:54:21 -0300

    Refactor dataset and model settings in 04_test_jb.py
    Updated dataset loading and model configuration for Gemma.

--- 04_test_jb.py
+++ 04_test_jb.py
@@ -1,30 +1,38 @@
 from openai import OpenAI
-from datasets import Dataset, load_from_disk, concatenate_datasets
+from datasets import Dataset, load_dataset, load_from_disk, concatenate_datasets
 import os
 
+os.environ["HF_HOME"] = "/raid/user_iago/guardrail/mijabench/hf_cache"
+os.environ["HF_DATASETS_CACHE"] = "/raid/user_iago/guardrail/mijabench/hf_cache/datasets"
 
-ds = load_from_disk('toxsyn_processed_something')
 
-MODEL_NAME="Qwen/Qwen3-14B-AWQ"
+# dataset = load_dataset("iagoalves/jailbreaking_toxsyn_qwen235_with_id")['train']
+dataset = load_dataset("iagoalves/jailbreaking_toxsyn_gemma")['train']
+
+# MODEL_NAME="meta-llama/Meta-Llama-3.1-8B-Instruct"
+MODEL_NAME="google/gemma-3-27b-it"
 
 MAX_TOKENS=2048
 TEMPERATURE=0.6
 TOP_P=0.9
-TOP_K=20
-BATCH_SIZE = 64
+TOP_K=1
+BATCH_SIZE = 2048
 
 
 
 client = OpenAI(
-    base_url="http://localhost:8000/v1",
+    # base_url="http://localhost:8000/v1",
+    base_url="http://dgx-H100-02:8000/v1",
     api_key="EMPTY"
 )
 
+print('\n\n\nCARREGOU_CLIENT\n\n\n')
 
 # Function to get model outputs
 def get_model_output(prompts, max_tokens=MAX_TOKENS, temperature=TEMPERATURE, top_p=TOP_P, top_k=TOP_K):
     if type(prompts) == str:
         prompts = list(prompts)
+
     completion = client.completions.create(
         model=MODEL_NAME,
         prompt=prompts,
@@ -42,10 +50,8 @@ def get_model_output(prompts, max_tokens=MAX_TOKENS, temperature=TEMPERATURE, to
 
 def get_prompt(text, MODEL_NAME=MODEL_NAME):
     model_name = MODEL_NAME.lower()
-    if 'qwen3' in model_name:
-        prompt = f"""<|im_start|>user\n{text} /no_think<|im_end|>\n<|im_start|><assistant><think>\n\n</think>"""
-    elif 'llama-3' in model_name:
-        prompt = f"""<|begin_of_text|><|start_header_id|>user<|end_header_id|>\n\n{text}<|eot_id|><|start_header_id|>assistant<|end_header_id|>"""
+    # prompt = f"""<|begin_of_text|><|start_header_id|>user<|end_header_id|>\n\n{text}<|eot_id|><|start_header_id|>assistant<|end_header_id|>"""
+    prompt = f"""<bos><start_of_turn>user\n{text}<end_of_turn>\n<start_of_turn>model\n""" # gemma 3
     return prompt
 
 
@@ -61,12 +67,13 @@ def answer_jb_prompt(jb_prompts):
 
 def full_pipeline(batch):
     jb_responses = answer_jb_prompt(batch['jb_prompt'])
-    batch['jb_response'] = jb_responses
+    model_id = MODEL_NAME.split('/')[-1]
+    batch[f'jb_response_{model_id}'] = jb_responses
     return batch
 
 
-dataset = load_from_disk('toxsyn_processed_something')
-dataset = dataset.select(range(110, 120))
-dataset = dataset.map(full_pipeline, batched=True, batch_size=BATCH_SIZE)
 
-dataset.save_to_disk('toxsyn_processed_something_llama8boutput')
+print('\n\n\nCOMEÇOU O MAP\n\n\n')
+dataset = dataset.map(full_pipeline, batched=True, batch_size=BATCH_SIZE)
+dataset.push_to_hub('iagoalves/jailbreaking_toxsyn_gemma', private=False)
+# dataset.push_to_hub('iagoalves/jailbreaking_toxsyn_llama8boutput', private=False)
